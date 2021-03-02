5b37eb434d98bd29e61e8ec40d4f00d6caaee2d0
Harshitha Arul Murugan <harshitha.arulm@gmail.com>
2021-03-02 15:47:48 -0800

    edited myTeam and made one agent offense, and the other one hybrid (which switches to offense when scared but otherwise acts as defense)

--- myTeam.py
+++ myTeam.py
@@ -4,6 +4,107 @@ from pacai.core import distance
 from pacai.core.directions import Directions
 
 
+class OffenseAgent(CaptureAgent):
+
+    def __init__(self, index, **kwargs):
+        super().__init__(index)
+
+    def registerInitialState(self, gameState):
+        super().registerInitialState(gameState)
+        self.isRed = gameState.isOnRedTeam(self.index)
+        # Your initialization code goes here, if you need any.
+
+    def chooseAction(self, gameState):
+        # Collect legal moves.
+        legalMoves = gameState.getLegalActions(self.index)
+        if Directions.STOP in legalMoves:
+            legalMoves.remove(Directions.STOP)
+
+        # Choose one of the best actions.
+        scores = [self.evaluationFunction(gameState, action) for action in legalMoves]
+        bestScore = max(scores)
+        bestIndices = [index for index in range(len(scores)) if scores[index] == bestScore]
+        chosenIndex = random.choice(bestIndices)  # Pick randomly among the best.
+        return legalMoves[chosenIndex]
+
+    def getOppFood(self, gameState):
+        if self.isRed:
+            return gameState.getBlueFood()
+        else:
+            return gameState.getRedFood()
+
+    def getOppGhost(self, gameState):
+        """
+        Returns list of opponent ghost POSITIONS
+        """
+        if self.isRed:
+            blueIndices = gameState.getBlueTeamIndices()
+            oppPos = [gameState.getAgentPosition(idx) for idx in blueIndices]
+            blueGhostPos = [pos for pos in oppPos if gameState.isOnBlueSide(pos)]
+            return blueGhostPos
+        else:
+            redIndices = gameState.getRedTeamIndices()
+            oppPos = [gameState.getAgentPosition(idx) for idx in redIndices]
+            redGhostPos = [pos for pos in oppPos if gameState.isOnRedSide(pos)]
+            return redGhostPos
+
+    def getOppGhostState(self, gameState):
+        """
+        Returns list of opponent ghost states
+        """
+        if self.isRed:
+            blueGhostState = []
+            blueIndices = gameState.getBlueTeamIndices()
+            for idx in blueIndices:
+                if gameState.isOnBlueSide(gameState.getAgentPosition(idx)):
+                    blueGhostState.append(gameState.getAgentState(idx))
+            return blueGhostState
+        else:
+            redGhostState = []
+            redIndices = gameState.getRedTeamIndices()
+            for idx in redIndices:
+                if gameState.isOnRedSide(gameState.getAgentPosition(idx)):
+                    redGhostState.append(gameState.getAgentState(idx))
+            return redGhostState
+
+    def onOppositeSide(self, position, gameState):
+        if self.isRed:
+            return gameState.isOnBlueSide(position)
+        else:
+            return gameState.isOnRedSide(position)
+
+    def evaluationFunction(self, currentGameState, action):
+
+        successorGameState = currentGameState.generateSuccessor(self.index, action)
+
+        # Useful information you can extract.
+        newPosition = successorGameState.getAgentPosition(self.index)
+        oldFood = self.getOppFood(currentGameState)
+        newGhostStates = self.getOppGhostState(successorGameState)
+        newGhostPositions = self.getOppGhost(successorGameState)
+
+        score = currentGameState.getScore() * 100  # weighted current score
+        # closestDistToFood = float('-inf')
+        if self.onOppositeSide(newPosition, successorGameState):
+            score += 100
+        for food in oldFood.asList():
+            dist = distance.maze(newPosition, food, currentGameState)
+            if dist == 0:  # check if the new position is food
+                score += 1000
+            else:  # add reciprocol manhattan dist to food (b/c close food = more points) * weight
+                score += (1 / dist) * 100
+        # calculate ghost score
+        eat_ghost = 1  # coefficient to switch values if ghost are edible
+        for i in range(len(newGhostPositions)):
+            if newGhostStates[i].getScaredTimer() > 0:  # if ghosts edible, change weight to inverse reciprocol
+                eat_ghost = -0.1
+            if distance.manhattan(newGhostPositions[i], newPosition) <= 1:  # if near ghost run away
+                score -= 50 * eat_ghost
+            else:  # subtract reciprocol ghost distance (b/c closer ghost = less points) * weight
+                score -= (1 / distance.manhattan(newGhostPositions[i], newPosition)) * 50 * eat_ghost
+        return score
+
+
 class DefenseAgent(CaptureAgent):
 
     def __init__(self, index, **kwargs):
@@ -196,9 +297,11 @@ class HybridAgent(CaptureAgent):
 
         # successorGameState = currentGameState.generateSuccessor(self.index, action)
         # newPosition = successorGameState.getAgentPosition(self.index)
-        oldOppPacmanPositions = self.getOppPacman(currentGameState)
+        # oldOppPacmanPositions = self.getOppPacman(currentGameState)
         evalFunc = self.defenseEvaluationFunction
-        if len(oldOppPacmanPositions) < 2:
+        isScared = currentGameState.getAgentState(self.index).getScaredTimer() > 0
+        # if len(oldOppPacmanPositions) < 2:
+        if isScared:
             evalFunc = self.offenseEvaluationFunction
         return evalFunc(currentGameState, action)
 
@@ -245,20 +348,18 @@ class HybridAgent(CaptureAgent):
         # calculate ghost score
         eat_ghost = 1  # coefficient to switch values if ghost are edible
         for i in range(len(newGhostPositions)):
-            if newGhostStates[i].getScaredTimer() > 0:  # if ghosts edible
+            if newGhostStates[i].getScaredTimer() > 0:  # if ghosts edible, change weight to inverse reciprocol
                 eat_ghost = -0.1
-            dist = distance.manhattan(newGhostPositions[i], newPosition)
-            if dist <= 1:  # if near ghost run away
+            if distance.manhattan(newGhostPositions[i], newPosition) <= 1:  # if near ghost run away
                 score -= 50 * eat_ghost
             else:  # subtract reciprocol ghost distance (b/c closer ghost = less points) * weight
-                score -= (1 / dist) * 50 * eat_ghost
+                score -= (1 / distance.manhattan(newGhostPositions[i], newPosition)) * 50 * eat_ghost
         return score
 
 
 def createTeam(firstIndex, secondIndex, isRed,
-               first= HybridAgent,
-               second= DefenseAgent):
-
+               first=HybridAgent,
+               second=DefenseAgent):
     firstAgent = first
     secondAgent = second
 
